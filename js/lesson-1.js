cd842dcdfcbe176860e95e995067759b69ab7c5d
uty <fgmapam@gmail.com>
2025-03-16 15:10:22 +0200

    update lesson-1.js

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -1,9 +1,19 @@
-// Запропонуйте користувачу ввести число
-let number = Number(prompt("Введіть число:"));
+
+const number = Number(prompt("Введіть число:"));
 
 // Перевіряємо, чи число дорівнює 10
 if (number === 10) {
     alert("Вірно");
 } else {
     alert("Невірно");
+}
+
+function removeCharacter(str, char){
+    let newstr = ''
+    for( let i = 0; i < str.length; i += 1){
+        if(str[i] !== char){
+            newstr+= str[i]
+        }
+    }
+    return newstr
 }
